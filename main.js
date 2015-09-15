15a104ab60f9dfd2e3cf8679df7d00213a8f0016
Jessey Eagan <jesseyeagan@Jesseys-MacBook-Air.local>
2015-09-15 09:01:36 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// var letterCapitalize = function(str) {
+// 	return str.charAt(0).toUpperCase();
+	
+// }
+
+// 	console.log(letterCapitalize("i love the dallas cowboys."))
+
+	
+
+
+	var wordCount = function(str) {
+		return str.split(' ')
+	
+		
+
+	}
+	console.log(wordCount("i love the dallas cowboys.").length)
